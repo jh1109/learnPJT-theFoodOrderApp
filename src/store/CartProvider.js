096ea0cfb577c004904dee6ee4aacab72984e408
Juhee Noh <nohjuhee227@gmail.com>
2023-06-16 03:11:48 +0900

    learn. 장바구니 상태 관리-Reducer(153)

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 // Provider 사이에 children을 지정함으로써,
 // 하위에 속하는 컴포넌트에 해당 context를 모두 제공할 수 있게 함을 의미한다.
 const CartProvider = ({ children }) => {
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    // dispatch함수의 형식은 내가 만드는 것이다.
+    dispatchCartAction({ type: "ADD", item: item });
+  };
   const removeItemFromCartHandler = (id) => {};
 
   // 기본값
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
