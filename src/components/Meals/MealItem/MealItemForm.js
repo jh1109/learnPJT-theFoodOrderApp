f1e2abb7e43e013218eab8ef59bd50b95b7be0e1
Juhee Noh <nohjuhee227@gmail.com>
2023-06-16 03:55:55 +0900

    learn. forwardRef로 수량값 가져오기(154)

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
-const MealItemForm = ({ id }) => {
+const MealItemForm = ({ onAddToCart, id }) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    onAddToCart(enteredAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        // 컴포넌트에는 ref를 지정해서 바로 사용할 수 없다.
+        // 해결방법 => Forward Refs
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + id,
@@ -18,6 +40,7 @@ const MealItemForm = ({ id }) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>수량은 1~5 사이의 값으로 입력하세요.</p>}
     </form>
   );
 };
