647fd333d00928c7ad6b0422fda0e88c308e5046
Juhee Noh <nohjuhee227@gmail.com>
2023-06-15 09:46:14 +0900

    learn. Header 컴포넌트 생성 및 디자인(142)

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        {/* URL이라면 src='URL 주소'로 작성하면 된다. */}
+        <img src={mealsImage} alt="맛있는 음식이 가득한 테이블" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
