305b2a3686cc64107b7aaf2578f0459a4223e617
Juhee Noh <nohjuhee227@gmail.com>
2023-06-17 22:59:46 +0900

    learn. Cart에서 아이템 수량 감소시키기(157)

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,6 +8,8 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     ); //number
@@ -26,9 +28,26 @@ const cartReducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
 
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
@@ -49,7 +68,9 @@ const CartProvider = ({ children }) => {
     // dispatch함수의 형식은 내가 만드는 것이다.
     dispatchCartAction({ type: "ADD", item: item });
   };
-  const removeItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   // 기본값
   const cartContext = {
