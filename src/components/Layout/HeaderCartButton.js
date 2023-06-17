2cadf4674f881f66da84832e35302fe6c1dacb2f
Juhee Noh <nohjuhee227@gmail.com>
2023-06-17 23:37:48 +0900

    learn. 장바구니에 추가할 때 마다 장바구니 버튼 bump 효과(158)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 // import Cart from "../Cart/Cart";
@@ -6,6 +6,8 @@ import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ onClick }) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   // const [showCart, setShowCart] = useState(false);
   // const cartHandler = () => {
   //   setShowCart(true);
@@ -25,10 +27,30 @@ const HeaderCartButton = ({ onClick }) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return;
+    // console.log("side effect");
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    // 애니메이션 재생시간이 300밀리초여서 setTimeout도 300으로 설정
+
+    // 장바구니 추가를 시간간격없이 매우 연속적으로 하는 경우에
+    // cleanup 함수를 통해 이전 타이머를 제거할 수 있음
+    return () => {
+      // console.log("clean up");
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
     // <Fragment>
     // {showCart && <Cart onClose={closeCartHandler} />}
-    <button type="button" className={classes.button} onClick={onClick}>
+    <button type="button" className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
